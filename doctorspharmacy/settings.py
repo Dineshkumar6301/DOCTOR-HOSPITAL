cb201d4fd5d1c02f29d8543a246ce475a58e929c
Dineshkumar6301 <dk967427@gmail.com>
2025-12-25 10:25:35 +0530

    Update

--- doctorspharmacy/settings.py
+++ doctorspharmacy/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv(
 
 DEBUG = True
 
-ALLOWED_HOST=("*")
+ALLOWED_HOSTS = ["doctor-hospital.onrender.com", "localhost", "127.0.0.1"]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
