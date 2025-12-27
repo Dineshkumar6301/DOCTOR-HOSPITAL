7ab84558f0889a161c7f033ed6c7bc8d02273ac3
Dineshkumar6301 <dk967427@gmail.com>
2025-12-27 12:22:43 +0530

    update

--- doctorspharmacy/settings.py
+++ doctorspharmacy/settings.py
@@ -65,13 +65,26 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv()
 
-DATABASES = {
-    "default": dj_database_url.config(
-        default=os.getenv("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=False,
-    )
-}
+import os
+
+ENVIRONMENT = os.getenv("ENVIRONMENT", "local")
+
+if ENVIRONMENT == "production":
+    import dj_database_url
+    DATABASES = {
+        "default": dj_database_url.config(
+            default=os.environ.get("DATABASE_URL"),
+            conn_max_age=600,
+            ssl_require=True
+        )
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 
