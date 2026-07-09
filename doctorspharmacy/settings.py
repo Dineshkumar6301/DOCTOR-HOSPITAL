0161c1fd6b5adfe4dfea8c054d37c495e04ee354
Dineshkumar6301 <dk967427@gmail.com>
2026-07-09 14:12:37 +0530

    udpate

--- doctorspharmacy/settings.py
+++ doctorspharmacy/settings.py
@@ -78,12 +78,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "doctorspharmacy.wsgi.application"
 
+import dj_database_url
+from decouple import config
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.parse(
+        config("DATABASE_URL")
+    )
 }
 
 
